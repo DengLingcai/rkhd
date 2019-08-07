c8d82e1c3ffc1e2762ae37ce417ae533fe7a9bef
DengLingcai <denglingcai18@outlook.com>
2019-08-07 10:06:47 +0800

    1.延时延期申请触发器 2.timesheet触发器

--- src/other/rkhd/salesbeforeapply/DateUtil.java
+++ src/other/rkhd/salesbeforeapply/DateUtil.java
@@ -20,35 +20,53 @@ public class DateUtil {
      * @param count    天数
      * @return  计算后的日期 yyyy-MM-dd
      */
-    static String getAfterDay(String startDay, int count) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            Date date = sdf.parse(startDay);
-            Calendar cl = Calendar.getInstance();
-            cl.setTime(date);
-            cl.add(Calendar.DATE, count);
-            return sdf.format(cl.getTime());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return "";
+    static long getAfterDay(long startDay, int count) {
+        Calendar cl = Calendar.getInstance();
+        cl.setTime(new Date(startDay));
+        cl.add(Calendar.DATE, count);
+        return cl.getTime().getTime();
     }
-
 
     /**
      * 获取当前系统日期
      * @return  当前系统日期  yyyy-MM-dd
      */
-    static String getSysDate() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    static long getSysDate() {
         Calendar cl = Calendar.getInstance();
-        return sdf.format(cl.getTime());
+        return cl.getTime().getTime();
+    }
+
+    /**
+     * 日期字符串转换为时间戳
+     * @param strDate 日期字符串
+     * @return 日期时间戳
+     */
+    static long strDateToLong(String strDate){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd hh:mm");
+        try {
+            Date date = format.parse(strDate);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            return calendar.getTime().getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return new Date().getTime();
     }
 
 
- /*   public static void main(String[] args) {
-        System.out.println(getSysDate());
+   public static void main(String[] args) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd hh:mm");
+       try {
+           Date date = format.parse("2019-09-06 00:00");
+           Calendar calendar = Calendar.getInstance();
+           calendar.setTime(date);
+           System.out.println(calendar.getTime());
+       } catch (ParseException e) {
+           e.printStackTrace();
+       }
+
     }
-*/
+
 
 }
--- src/other/rkhd/salesbeforeapply/SalesBeforeApplyTrigger.java
+++ src/other/rkhd/salesbeforeapply/SalesBeforeApplyTrigger.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,7 +34,6 @@ public class SalesBeforeApplyTrigger implements Trigger {
 
     private Logger logger = LoggerFactory.getLogger();
 
-
     @Override
     public TriggerResponse execute(TriggerRequest triggerRequest) throws ScriptBusinessException {
         List<DataResult> dataResults = new ArrayList< DataResult >();
@@ -44,60 +44,83 @@ public class SalesBeforeApplyTrigger implements Trigger {
         if (extensionRequestList != null && extensionRequestList.size() > 0) {
             for (XObject xObject : extensionRequestList) {
                 //获取延期延时申请中的wbscode
-                String wbsCode = xObject.getAttribute("customItem1__c");
-                logger.info("---------------get 获取wbscode:"+wbsCode);
+                Long wbsCode = xObject.getAttribute("customItem1__c");
+                logger.info("---------------get wbscode:"+wbsCode);
                 /**
                  * 根据wbscode获取到所有的超时x工时 求和
                  */
                 querySql = " select customItem3__c,customItem5__c  from customEntity15__c where customItem1__c =  '" + wbsCode + "' order by id asc   ";
                 try {
                     String pageResult = HttpUtil.sentRequestGetResult("POST","/data/v1/query","q",querySql + " limit 0,300 ");
+                    logger.info("------------first query pageResult:"+pageResult);
                     if (StringUtils.isNotBlank(pageResult)){
                         JSONObject pageResultJson = JSONObject.parseObject(pageResult);
                         String totalSize = pageResultJson.get("totalSize").toString();
                         String count = pageResultJson.get("count").toString();
+                        logger.info("----------------totalSize:"+totalSize+":--------count:"+count);
                         int pageSize = new BigDecimal("".equals(totalSize) ? "0" : totalSize).divide(new BigDecimal("".equals(count) ? "0" : count)).setScale(0, BigDecimal.ROUND_UP).intValue();
+                        logger.info("----------------pageSize:"+pageSize);
                         for (int i = 0; i < pageSize; i ++){
-                            String pageResult1 = HttpUtil.sentRequestGetResult("POST","/data/v1/query","q",querySql + " limit 300*"+i+",300 ");
+                            int pageNo = 300 * i;
+                            String pageResult1 = HttpUtil.sentRequestGetResult("POST","/data/v1/query","q",querySql + " limit "+pageNo+",300 ");
+                            logger.info("------------seconed query pageResult:"+pageResult1+"---");
                             JSONObject pageResultJson1 = JSONObject.parseObject(pageResult1);
-                            JSONArray jsonArray = JSONArray.parseArray(pageResultJson1.get("records").toString());
-                            int size = jsonArray.size();
-                            for (int j = 0; j < size; j++) {
-                                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                                //超时工时
-                                String overtimeWork  = jsonObject.get("customItem3__c").toString();
-                                allTimeWork = new BigDecimal(allTimeWork).add(new BigDecimal(overtimeWork)).intValue();
-                                //延期天数
-                                String overDay  = jsonObject.get("customItem5__c").toString();
-                                allOverDays = new BigDecimal(allOverDays).add(new BigDecimal(overDay)).intValue();
+                            logger.info("------------seconed query pageResultJson1:"+pageResultJson1+"---");
+                            if (pageResultJson1 != null){
+                                Object object =  pageResultJson1.get("records") ;
+                                logger.info("------------seconed query object:"+object+"---");
+                                JSONArray jsonArray = JSON.parseArray(object== null ? "" : object.toString());
+                                logger.info("------------seconed query jsonArray:"+jsonArray+"---");
+                                int size = jsonArray.size();
+                                for (int j = 0; j < size; j++) {
+                                    JSONObject jsonObject = (JSONObject) jsonArray.get(j);
+                                    //超时工时
+                                    String overtimeWork  = jsonObject.get("customItem3__c").toString();
+                                    allTimeWork = new BigDecimal(allTimeWork).add(new BigDecimal(overtimeWork)).intValue();
+                                    //延期天数
+                                    String overDay  = jsonObject.get("customItem5__c").toString();
+                                    allOverDays = new BigDecimal(allOverDays).add(new BigDecimal(overDay)).intValue();
+                                }
                             }
                         }
                     }
+                    logger.info("------------//超时工时:"+allTimeWork+"----延期天数:"+allOverDays);
                     /**
                      * update wbscode 中的  已批准超支工时（小时） 已批准延期（天数） 当前结束日期
                      */
-                    querySql = "select customItem17__c from  customEntity7__c where id = " + wbsCode ;
-                    String pageResult2 = HttpUtil.sentRequestGetResult("POST","q","/data/v1/query",querySql);
+                    querySql = "select customItem15__c from  customEntity7__c where id = " + wbsCode ;
+                    String pageResult2 = HttpUtil.sentRequestGetResult("POST","/data/v1/query","q",querySql);
                     JSONObject pageResultJson1 = JSONObject.parseObject(pageResult2);
                     JSONArray jsonArray = JSONArray.parseArray(pageResultJson1.get("records").toString());
                     int size = jsonArray.size();
-                    String nowEndDate = "";
+                    Long nowEndDate = new Date().getTime();
                     for (int j = 0; j < size; j++) {
                         JSONObject jsonObject = (JSONObject) jsonArray.get(j);
-                         //wbscode的当前结束日期
-                         nowEndDate  = jsonObject.get("customItem17__c").toString();
+                         //wbscode的原始结束日期
+                         nowEndDate  =DateUtil.strDateToLong( jsonObject.get("customItem15__c").toString() );
                     }
-                    nowEndDate = "".equals(nowEndDate) || nowEndDate == null ? DateUtil.getAfterDay(DateUtil.getSysDate(),allOverDays) : DateUtil.getAfterDay(nowEndDate,allOverDays) ;
+                    logger.info("------------//nowEndDate:"+nowEndDate);
+                    nowEndDate = nowEndDate == null ? DateUtil.getAfterDay(DateUtil.getSysDate(),allOverDays) : DateUtil.getAfterDay(nowEndDate,allOverDays) ;
+                    logger.info("------------//nowEndDate22:"+nowEndDate);
+                    JSONObject jsonObject = new JSONObject();
                     JSONObject jsonObject1 = new JSONObject();
                     jsonObject1.put("customItem25__c",allTimeWork);
                     jsonObject1.put("customItem16__c",allOverDays);
                     jsonObject1.put("customItem17__c",nowEndDate);
-                    String pageResult1 = HttpUtil.sentRequestGetResult("PATCH","/rest/data/v2/objects/customEntity7__c/"+wbsCode,"data",jsonObject1.toString());
+                    jsonObject.put("data",jsonObject1);
+                    //String pageResult1 = HttpUtil.sentRequestGetResult("PATCH","https://api.xiaoshouyi.com/rest/data/v2/objects/"+wbsCode,"data",jsonObject1.toString());
+                    RkhdHttpClient rkhdHttpClient = new RkhdHttpClient();
+                    RkhdHttpData rkhdHttpData = new RkhdHttpData();
+                    rkhdHttpData.setCall_type("PATCH");
+                    rkhdHttpData.setCallString("/rest/data/v2/objects/customEntity7__c/"+wbsCode);
+                    rkhdHttpData.setBody(jsonObject.toString());
+                    //rkhdHttpData.putFormData("data",jsonObject1.toString());
+                    String pageResult1 = rkhdHttpClient.performRequest(rkhdHttpData);
                     String httpCode = JSONObject.parseObject(pageResult1).get("code").toString();
                     if ("200".equals(httpCode)) {
                         logger.info("=======================================update success："+jsonObject1.toString());
                     }else {
-                        logger.info("=======================================update fail："+jsonObject1.toString());
+                        logger.info("=======================================update fail："+JSONObject.parseObject(pageResult1).get("msg").toString());
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
